d9fe5b0ab53780e79815630af91da417f080c36a
coloured man <101802611+snc45@users.noreply.github.com>
2023-07-23 16:33:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference=0;
+rightWristX = 0;
+leftWristX=0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet =ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized ');
+}
+
+function draw() {
+    backgroumd('#969A97');
+}
+
+function gotPoses(results){
+    if(results.lenght>0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX +" noseY = "+noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX)
+
+    }
+}
+
+function draw () {
+
+    
+
+    background('#969A97');
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
